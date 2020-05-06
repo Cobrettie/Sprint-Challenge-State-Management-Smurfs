b436924af81c1b8b269c25e3980768c8fb38ad3e
Cobrettie <cobrettiegarner3@gmail.com>
2020-05-06 17:59:14 -0400

    create directories for actions/reducers

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -5,6 +5,7 @@ import { createStore } from 'redux';
 import App from "./components/App";
 import "./index.css";
 
+const store = createStore()
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
