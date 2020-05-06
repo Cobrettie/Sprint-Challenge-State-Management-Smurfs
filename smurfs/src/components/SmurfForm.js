2d684220363e85b70442740f8b82df783336e79d
Cobrettie <cobrettiegarner3@gmail.com>
2020-05-05 20:36:53 -0400

    POST request to api successful

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,26 +1,41 @@
 import React from 'react';
+import axios from 'axios';
 
 class SmurfForm extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      value: ''
+      name: '',
+      age: 0,
+      height: 'cm'
     }
 
   }
 
   handleChange = event => {
-    this.setState({ value: event.target.value })
+    this.setState({[event.target.name] : event.target.value})
   }
 
   handleSubmit = event => {
     event.preventDefault();
-    console.log('form submittied')
+    console.log('form submitted')
+    this.addSmurf()
+  }
+
+  addSmurf = () => {
+    axios.post('http://localhost:3333/smurfs', {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    })
+      .then(response => {
+        console.log('post req response', response)
+      })
   }
 
   render() {
-    console.log('state.value', this.state.value)
+    console.log('state', this.state)
     return (
       <div>
       <form onSubmit={this.handleSubmit}>
@@ -28,18 +43,22 @@ class SmurfForm extends React.Component {
           type='text'
           name='name'
           // placeholder='name'
-          value={this.state.value}
+          value={this.state.name}
           onChange={this.handleChange}
         />
         <input 
           type='text'
           name='age'
           placeholder='age'
+          value={this.state.age}
+          onChange={this.handleChange}
         />
         <input 
           type='text'
           name='height'
           placeholder='height'
+          value={this.state.height}
+          onChange={this.handleChange}
         />
         <button type='submit'>Submit</button>
       </form>
